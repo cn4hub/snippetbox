a72314007913483350e263b150fc7b11a1a49e9f
David Kremzow <david.kremzow@gmail.com>
2025-09-18 14:34:30 +0200

    Multiple changes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,38 +2,27 @@ package main
 
 import (
 	"flag"
-	"log"
 	"log/slog"
 	"net/http"
 	"os"
 	"strings"
 )
 
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "http service address")
 	flag.Parse()
 
-	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	app := &application{
+		logger: slog.New(slog.NewJSONHandler(os.Stdout, nil)),
+	}
 
-	mux := http.NewServeMux()
-
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// Note that the path given to the http.Dir function is relative to the project
-	// directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("GET /static/", http.StripPrefix("/static", neuter(fileServer)))
-
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-	mux.HandleFunc("GET /snippet/create", snippetCreate)
-	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
-	log.Print("starting server on :4000")
-	logger.Info("starting server", slog.Any("addr", *addr))
-	err := http.ListenAndServe(*addr, mux)
-	logger.Error(err.Error())
+	app.logger.Info("starting server", slog.Any("addr", *addr))
+	err := http.ListenAndServe(*addr, app.routes())
+	app.logger.Error(err.Error())
 	os.Exit(1)
 }
 
